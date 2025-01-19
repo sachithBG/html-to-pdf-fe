b356bbbb0fdda52016132e9999f94073c6c4e27f
BG-Sachith <bs.guruge@gmail.com>
2025-01-20 00:17:40 +0530

    updates

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/';
+export const baseURL = process.env.NEXT_PUBLIC_API_URL || '188.166.245.108:4000/api/';
 
 export const API = axios.create({ baseURL: baseURL+'v1' });
