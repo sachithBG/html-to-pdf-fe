fc6608f24b920b32d009c4b207c7d50343c0586c
BG-Sachith <bs.guruge@gmail.com>
2025-01-20 00:09:32 +0530

    updates

--- src/app/(pages)/setup/media/page.tsx
+++ src/app/(pages)/setup/media/page.tsx
@@ -8,7 +8,7 @@ import { findAllAddons } from '@/app/services/addonService';
 import { getDefaultOrganization, Organization, OrganizationState } from '@/redux/slice/organizationSlice';
 import { useSelector } from 'react-redux';
 import { useSnackbar } from 'notistack';
-import ImageManage from '../components/ImageManage';
+import ImageManage from '../components/ImageArrange';
 // const ImageManage = dynamic(() => import('../components/ImageManage'), { ssr: false });
 const ImageList = dynamic(() => import('../components/ImageList'), { ssr: false });
 
