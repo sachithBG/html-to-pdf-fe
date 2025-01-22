cfe434729a32b91be5940d645d61423e75c48d2d
BG-Sachith <bs.guruge@gmail.com>
2025-01-22 12:50:18 +0530

    fixes

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const API = axios.create({
-    baseURL: `${baseURL || "http://localhost:4000/api_/"}v1`,
+    baseURL: `${baseURL || "//localhost:4000/api_/"}v1`,
     timeout: 30000, // Optional: 10 seconds timeout
     headers: {
         "Content-Type": "application/json",
