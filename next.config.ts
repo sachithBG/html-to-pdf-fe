1dcb090f1b8c77110ef13359bf49117218c8d783
BG-Sachith <bs.guruge@gmail.com>
2025-01-23 07:27:25 +0530

    builder

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,15 @@ const nextConfig: NextConfig = {
   reactStrictMode: false, // Enables React Strict Mode
   // swcMinify: true, // Uses the faster SWC compiler for minification
   pageExtensions: ["tsx", "ts"],
-  // webpack: (config, options) => {
-  //   // Check if the build is in production
-  //   if (!options.dev) {
-  //     // For server-side builds, no source maps
-  //     config.devtool = options.isServer ? false : 'cheap-module-source-map'; // Or use any other devtool setting that suits you
-  //   }
-  //   config.resolve.alias['@'] = path.resolve(path.resolve());
-  //   return config;
-  // },
+  webpack: (config, options) => {
+    // Check if the build is in production
+    if (!options.dev) {
+      // For server-side builds, no source maps
+      config.devtool = options.isServer ? false : 'cheap-module-source-map'; // Or use any other devtool setting that suits you
+    }
+    config.resolve.alias['@'] = path.resolve(path.resolve());
+    return config;
+  },
   compiler: {
     // removeConsole: true,
   },
